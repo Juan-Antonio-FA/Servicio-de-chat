57a71553732d8e0044c475704f1efef14c717c57
Juan <dxl.juan12@gmail.com>
2024-05-18 15:06:30 -0600

    First commit

---
+++ Servicio_de_chat/src/main/java/com/mycompany/servicio_de_chat/Servicio_de_chat.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.servicio_de_chat;
+/**
+ *
+ * @author Juan
+ */
+public class Servicio_de_chat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
